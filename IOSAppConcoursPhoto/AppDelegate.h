db81b4cdeb71052b1df8dcb57675d02574b68e72
Kerautret <kerautre@loria.fr>
2012-10-22 23:33:15 +0200

    Initial Commit

---
+++ IOSAppConcoursPhoto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOSAppConcoursPhoto
+//
+//  Created by Kerautret on 22/10/12.
+//  Copyright (c) 2012 projetTutoreSRC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
