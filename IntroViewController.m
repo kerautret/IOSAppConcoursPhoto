a886ca260a0f2434dec905c4cd5b0a45a4cf8b9b
Kerautret <kerautre@loria.fr>
2012-11-06 00:20:22 +0100

    Rajout code appareil photo

---
+++ IntroViewController.m
@@ -0,0 +1,38 @@
+//
+//  IntroViewController.m
+//  IOSAppConcoursPhoto
+//
+//  Created by Kerautret on 05/11/12.
+//  Copyright (c) 2012 projetTutoreSRC. All rights reserved.
+//
+
+#import "IntroViewController.h"
+
+@interface IntroViewController ()
+
+@end
+
+@implementation IntroViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
